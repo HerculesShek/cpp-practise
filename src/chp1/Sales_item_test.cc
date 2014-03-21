107b42d853cc252d95325f7493bc39c1c8e2e8ec
HerculesShek <hercules.shek@gmail.com>
2014-03-21 08:33:51 +0800

    test Sales_item

--- src/chp1/Sales_item_test.cc
+++ src/chp1/Sales_item_test.cc
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "Sales_item.h"
-
+using namespace std;
 int main()
 {
-  Sales_item item;
-  std::cin >> item;
+  Sales_item book;
+  std::cin >> book;
 
-  std::cout << item << std::endl;
+  std::cout << book << std::endl;
 
+  Sales_item item1, item2;
+  std::cin >> item1 >> item2; // read a pair of transactions
+  if(item1.isbn() == item2.isbn())
+	std::cout << item1 + item2 << std::endl; // print their sum
+  else{
+	cerr << "error"<<endl;
+	return -1;
+  }
   return 0;
 }
  
