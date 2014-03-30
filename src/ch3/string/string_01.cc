558d83bb0b7ddfbf1108f251159b018850300d8c
HerculesShek <hercules.shek@gmail.com>
2014-03-30 16:13:16 +0800

    string test

---
+++ src/ch3/string/string_01.cc
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+int main(){
+  
+  string a = "aaa";
+
+  return 0;
+}
---
+++ src/chp2/class/includee_test01.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Sales_item.h"
+using namespace std;
+
+int main(){
+  Sales_item a, b;
+  cout << (a+b) << endl;
+  string name = "aaa";
+  cout << name << endl;
+
+
+  return 0;
+}
