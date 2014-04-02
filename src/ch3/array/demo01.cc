13e11725fdde69ef079b8b7227433910ba2b4c9d
HerculesShek <hercules.shek@gmail.com>
2014-04-02 15:17:39 +0800

    array

---
+++ src/ch3/array/demo01.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+unsigned scores[11] = {};
+unsigned as[11];
+int main()
+{
+  for(auto a : scores)
+	cout << a << endl;
+  
+  for(auto a : as)
+	cout << a << endl;
+  
+  return 0;
+}
