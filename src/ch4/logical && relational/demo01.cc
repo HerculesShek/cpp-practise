03ba64c5c2485948f0af7416c1f9c0c24ec61d82
HerculesShek <hercules.shek@gmail.com>
2014-04-07 14:50:42 +0800

    bool demo

--- src/ch4/logical && relational/demo01.cc
+++ src/ch4/logical && relational/demo01.cc
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
+  int a = 5;
+  if(a)
+	cout << "a is true" << endl;
+  
+  if(a == true)
+	cout << "a is 1" << endl;
+
+  a = 1;
+  if(a == true)
+	cout << "a is 1" << endl;
 
   return 0;
 }
