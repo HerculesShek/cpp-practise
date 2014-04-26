2efdfdf8a8dd6aa52381a140e7599111eeb341b8
HerculesShek <hercules.shek@gmail.com>
2014-04-26 23:41:47 +0800

    friend class

--- src/ch7/additional/Screen.h
+++ src/ch7/additional/Screen.h
@@ -5,16 +5,17 @@
 using std::string;
 
 class Screen{
- public:
+  friend class Window_mgr;
+public:
   typedef string::size_type pos;
   Screen() = default;
- Screen(pos ht, pos wd, char c):
-  height(ht), width(wd), contents(ht*wd, c){}
+  Screen(pos ht, pos wd, char c):
+    height(ht), width(wd), contents(ht*wd, c){}
   char get() const 
   {return contents[cursor]; }
   inline char get(pos ht, pos wd) const;
   Screen &move(pos r, pos c);
- private:
+private:
   pos cursor = 0;
   pos height, width = 0;
   string contents;
---
+++ src/ch7/additional/Window_mgr.h
@@ -0,0 +1,9 @@
+#ifndef WINDOWS_MGR_H
+#define WINDOWS_MGR_H
+
+class Windows_mgr{
+  
+  
+};
+
+#endif
