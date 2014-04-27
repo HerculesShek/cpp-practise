45ba47b53c4fe163a9a8b0f21f1d44a41208f3d5
HerculesShek <hercules.shek@gmail.com>
2014-04-27 23:21:01 +0800

    define Windows_mgr

--- src/ch7/additional/Window_mgr.h
+++ src/ch7/additional/Window_mgr.h
@@ -1,8 +1,13 @@
 #ifndef WINDOWS_MGR_H
 #define WINDOWS_MGR_H
+#include <vector>
 
 class Windows_mgr{
-  
+public:
+  using ScreenIndex = std::vector<string>::size_type;
+  void clear(ScreenIndex);
+private:
+  std::vector<Screen> screens{Screen(24, 80, ' ')};
   
 };
 
