8bfb92151358b30c589fe836cba9b8a266e08dd2
HerculesShek <hercules.shek@gmail.com>
2014-05-13 15:04:44 +0800

    map

---
+++ src/ch11/overview/map_demo.cc
@@ -0,0 +1,17 @@
+#include <map>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  map<string, size_t> word_count;
+  string word;
+  while(cin >> word)
+	++word_count[word];
+
+  for(const auto &w : word_count)
+	cout << w.first << " occurs " << w.second 
+		 << (w.second > 1 ? " times" : " time") << endl;
+  return 0;
+}
