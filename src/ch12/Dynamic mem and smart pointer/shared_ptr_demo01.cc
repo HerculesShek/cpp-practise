16ba0ff2a1adea6fcfcd5eaa667d1a63bde5fda6
HerculesShek <hercules.shek@gmail.com>
2014-05-15 16:23:25 +0800

    shared_ptr_demo

---
+++ src/ch12/Dynamic mem and smart pointer/shared_ptr_demo01.cc
@@ -0,0 +1,13 @@
+#include <memory>
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main()
+{
+  
+  shared_ptr<string> p1;
+  shared_ptr<list<int>> p2;
+  return 0;
+}
